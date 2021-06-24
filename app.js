095045d338ff535122579e3ab190216ad94b3cd1
Ahror0248 <akhrorcodrr@gmail.com>
2021-06-24 23:12:58 +0500

    done

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('tajriba')
+// task 1
+// for(var i=10; i>=0; i--) {
+//     // console.log(i)
+// }
+
+// task 2
+// for(let i=1; i<10; i++) {
+    // if(i%2!=0)
+    // console.log(i)
+// // }
+
+// task 3
+// for(let i=0; i<10; i++) {
+//     if(i%2==0)
+//     console.log(i)
+// }
+
+// task 4
+// for(let i=0; i<100; i++) {
+//     if(i%3==0)
+//     console.log(i)
+// }
+
+// task 5
+// for(let i=0; i<100; i++) {
+//     if(i%5==0 || i%3==0)
+//     console.log(i)
+// }
+
+
+
+// hafta = 55
+
+// switch(hafta) {
+//     case 1: console.log('Dushanba'); break
+//     case 2: console.log('Seshanba'); break
+//     case 3: console.log('Chorshanba'); break
+//     case 4: console.log('Payshanba'); break
+//     case 5: console.log('Juma'); break
+//     case 6: console.log('Shanba'); break
+//     case 7: console.log('Yakshanba'); break
+//     default: console.log('Bunday hafta kuni mavjud emas')
+// }
+
+
+// message = 10
+
+// message > 0 ? 
+//      message < 90 ? console.log('sizga kop yangi habar bor') :console.log('sizga oz yangi habar bor')
+//    : 
+//      console.log('Yangi habar yoq')
+
+let i = 0
+
---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+
+title = 'pdp academy'
+
+// console.log(title[0])
+// console.log(title[1])
+// console.log(title[0], title[1])
+
+let i=0
+for(i; i<= title.length; i+=2){
+    console.log(title[i])
+}
